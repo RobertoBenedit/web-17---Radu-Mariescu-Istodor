a50790865f8e57c7377e404840a364e877425179
RobertoBenedit <roberto.benedit.contador@gmail.com>
2022-06-11 00:27:33 -0300

    lecture 1

--- car.js
+++ car.js
@@ -5,30 +5,70 @@ class Car {
         this.width = width;
         this.height = height;
 
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
+
         this.controls = new Controls();
     }
     update() {
-        if (this.controls.left) {
-            this.x -= 5;
-        }
-        if (this.controls.right) {
-            this.x += 5;
-        }
+        this.#move();
+    }
+    #move() {
         if (this.controls.forward) {
-            this.y -= 5;
+            this.speed += this.acceleration;
         }
         if (this.controls.reverse) {
-            this.y += 5;
+            this.speed -= this.acceleration;
         }
+
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            }
+        }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+
+        this.y -= this.speed;
     }
+
     draw(ctx) {
+        let gradient = ctx.createLinearGradient(0, -25, 0, 25);
+        gradient.addColorStop(0, "black");
+        // gradient.addColorStop(0.7, "white");
+        gradient.addColorStop(1, "yellow");
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
-        ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
-            this.width,
-            this.height
-        );
+        ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+        ctx.fillStyle = gradient;
         ctx.fill();
+
+        ctx.restore();
     }
 }
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // @ts-nocheck
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 const ctx = canvas.getContext("2d");
 
 const car = new Car(100, 100, 30, 50);
 
 function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.height = window.innerHeight;
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
     car.update();
     car.draw(ctx);
     requestAnimationFrame(animate);
