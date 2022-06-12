2627724890a757745e5158c4d05e2a084f957cd4
Roberto Benedit <73159144+RobertoBenedit@users.noreply.github.com>
2022-06-12 21:58:04 +0000

    ultimo

--- road.js
+++ road.js
@@ -1,21 +1,11 @@
 class Road {
-=======
     contructor(x, width, laneCount = 3) {
->>>>>>> refs/remotes/origin/master
         this.x = x;
         this.width = width;
         this.laneCount = laneCount;
 
         this.left = x - width / 2;
-<<<<<<< HEAD
+
        this.right = x + width / 2;
     }
-
-    draw(ctx) {
-        ctx.fillStyle = "black";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-=======
-        this.right = x + width / 2;
-    }/remotes/origin/master
 }
