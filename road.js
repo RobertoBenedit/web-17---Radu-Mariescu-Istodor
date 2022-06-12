1a198e3233be842ac6639a62230911700dab780c
Roberto Benedit <73159144+RobertoBenedit@users.noreply.github.com>
2022-06-12 21:51:46 +0000

    last

--- road.js
+++ road.js
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-// draw a road for cars
-class Road {
-    contructor(x, width, laneCount = 3) {
-=======
 class Road {
     constructor(x, width, laneCount) {
->>>>>>> fbabfcb26b8bd5cb0cbbef2f87a900d2caa3e861
         this.x = x;
         this.width = width;
         this.laneCount = laneCount;
 
         this.left = x - width / 2;
-<<<<<<< HEAD
-=======
-        this.right = x + width / 2;
+       this.right = x + width / 2;
     }
 
     draw(ctx) {
         ctx.fillStyle = "black";
         ctx.fillRect(this.x, this.y, this.width, this.height);
->>>>>>> fbabfcb26b8bd5cb0cbbef2f87a900d2caa3e861
     }
 }
